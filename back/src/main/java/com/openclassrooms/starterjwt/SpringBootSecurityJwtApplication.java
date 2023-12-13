8f15487464dd45692d8ee5146dd8f6677ba047c5
Gaétan L <codebygaetan@gmail.com>
2023-12-13 16:09:16 +0100

    correctif test back end

--- back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
+++ back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
@@ -1,12 +1,17 @@
 package com.openclassrooms.starterjwt;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import lombok.Generated;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringBootSecurityJwtApplication {
+
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
 	}
